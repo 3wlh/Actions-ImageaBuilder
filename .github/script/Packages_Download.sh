095de052413bc1e471c0e1d1eda8d588cdde0cff
3wlh <3wlh@3wlh.cn>
2026-04-09 23:45:57 +0800

    更新 Packages_Download.sh

--- .github/script/Packages_Download.sh
+++ .github/script/Packages_Download.sh
@@ -2,17 +2,19 @@
 regexper='^(https?)://[a-zA-Z0-9.-]+\.[a-zA-Z0-9]{1,}(:[0-9]{1,5})?(/[^/]*)*$'
 dir="${2}"
 function download(){
-    curl -# -L --fail "${1}" -o "$(pwd)/$(basename ${1})"
+    curl -# -L --fail "${1}" -o "${dir}/$(basename ${1})"
     if [[ "$(du -b $(pwd)/$(basename ${1}) 2>/dev/null | awk '{print $1}')" -le "512" ]]; then
-		echo -e "$(date '+%Y-%m-%d %H:%M:%S')\e[1;31m - 【$(basename ${url})】下载失败\e[0m"
+		echo -e "$(date '+%Y-%m-%d %H:%M:%S')\e[1;31m - 【$(basename ${1})】下载失败\e[0m"
 		exit
 	fi
-	echo -e "$(date '+%Y-%m-%d %H:%M:%S')\e[1;32m - 【$(basename ${url})】下载完成\e[0m"
+	echo -e "$(date '+%Y-%m-%d %H:%M:%S')\e[1;32m - 【$(basename ${1})】下载完成\e[0m"
 }
 
 function handle(){
 	cat "${1}" 2>/dev/null | \
-	while IFS= read -r LINE; do
+	while IFS= read -r LINE || [ -n "$LINE" ]; do
+		[[ -z "${LINE// /}" ]] && continue
+		LINE="${LINE%$'\r'}"
 		if grep -qE "$regexper" <<< "${LINE}"; then
 			echo "Downloading ${LINE}"
 			[[ $(curl -o /dev/null -s --head -w "%{http_code}" "${LINE}") -ge 400 ]] && { echo "无法访问"; exit; }
@@ -20,4 +22,5 @@ function handle(){
 		fi
 	done
 }
+
 handle "${1}"
