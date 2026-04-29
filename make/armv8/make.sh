e430e7cebd575e84f81d9e464a31ba8411670390
3wlh <3wlh@3wlh.cn>
2026-04-30 00:03:16 +0800

    UP

--- make/armv8/make.sh
+++ make/armv8/make.sh
@@ -7,8 +7,8 @@ wget -q ${Script_url}/Default_Packages.sh -O "$(pwd)/def_pkg.env" && source "$(p
 find . -maxdepth 1 -type f -name "repositories.conf" -exec cp {} "$(pwd)/packages/" \;
 echo "============================= 初始化自定义插件 ============================="
 # 添加签名和插件源
-echo -e "untrusted comment: public key 29026b52f8ff825c\nRWQpAmtS+P+CXP4/60amOLDZs7jqKfTrFlKt5+UHYTU0ED9pRmh73vz7" >\
-"$(pwd)/keys/29026b52f8ff825c" && cp -f "$(pwd)/keys/29026b52f8ff825c" "$(pwd)/files/etc/opkg/keys/"
+key="untrusted comment: public key 29026b52f8ff825c\nRWQpAmtS+P+CXP4/60amOLDZs7jqKfTrFlKt5+UHYTU0ED9pRmh73vz7"
+echo -e "${key}" > "$(pwd)/keys/29026b52f8ff825c" 
 sed -i '1a src/gz 3wlh https://packages.11121314.xyz/packages/aarch64_generic' "repositories.conf"
 [[ -d "$(pwd)/packages/diy_packages" ]] || mkdir -p "$(pwd)/packages/diy_packages"
 [[ -f "$(pwd)/diy_config/diy_pkg.env" ]] && source "$(pwd)/diy_config/diy_pkg.env"
@@ -18,13 +18,10 @@ echo "============================= 初始化自定义配置 ===================
 [[ -z "${PROFILE}" ]] && { echo "获取编译设备配置失败！";exit 1; }
 export Model="${Model}"
 [[ -d "$(pwd)/files/etc/opkg/keys" ]] || mkdir -p "$(pwd)/files/etc/opkg/keys"
+cp -f "$(pwd)/keys/29026b52f8ff825c" "$(pwd)/files/etc/opkg/keys/"
 wget -qO- ${Script_url}/Diy_file_all.sh | bash
 [[ -f "$(pwd)/diy_config/defaults.sh" ]] && \
 cp -f "$(pwd)/diy_config/defaults.sh" "$(pwd)/files/etc/uci-defaults/99-defaults1.sh"
-echo "=========================== 查看下载插件 ==========================="
-ls -lhS "$(pwd)/packages/diy_packages/" | awk '{print $9,$5}'
-echo "=========================== 查看下载插件 ==========================="
-ls -lhS "$(pwd)/packages/diy_packages/" | awk '{print $9,$5}'
 echo "============================= 检查缓存 ============================="
 if [[ $(find "$(pwd)/dl" -type f 2>/dev/null | wc -l) -gt 0 ]]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - 正在检查缓存插件："
