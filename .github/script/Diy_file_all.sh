4f8918f232c17a93d3660040664db7980ac8ec00
3wlh <3wlh@3wlh.cn>
2026-04-29 23:43:48 +0800

    up

--- .github/script/Diy_file_all.sh
+++ .github/script/Diy_file_all.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-URL="https://raw.githubusercontent.com/3wlh/Actions-ImageaBuilder/refs/heads/main/diy_files/all/"
+URL="https://raw.githubusercontent.com/3wlh/Actions-ImageaBuilder/refs/heads/main/files/"
 
-[[ -d "$(pwd)/files/etc/uci-defaults" ]] || mkdir -p "$(pwd)/files/etc/uci-defaults"
+[ -d "$(pwd)/files/etc/uci-defaults" ] || mkdir -p "$(pwd)/files/etc/uci-defaults"
 wget -q "${URL}/99-defaults.sh" -O "$(pwd)/files/etc/uci-defaults/99-defaults.sh"
 wget -q "${URL}/sys-opkg.sh" -O "$(pwd)/files/etc/uci-defaults/sys-opkg.sh"
 wget -q "${URL}/sys-bash.sh" -O "$(pwd)/files/etc/uci-defaults/sys-bash.sh"
 
-[[ -d "$(pwd)/files/etc/profile.d" ]] || mkdir -p "$(pwd)/files/etc/profile.d"
+[ -d "$(pwd)/files/etc/profile.d" ] || mkdir -p "$(pwd)/files/etc/profile.d"
 wget -q "${URL}/sys-sysinfo.sh" -O "$(pwd)/files/etc/profile.d/sys-sysinfo.sh"
 
 mkdir -p "$(pwd)/files/www/luci-static/resources/view/status/include" &&
