56459db2a9e77379a8a82a94891d90fb3814ea94
3wlh <3wlh@3wlh.cn>
2026-04-29 23:47:46 +0800

    Update Download.sh

--- .github/script/Download.sh
+++ .github/script/Download.sh
@@ -3,6 +3,10 @@ dir="$(pwd)/packages/diy_packages"
 test -d ${dir} || mkdir -pm 755 ${dir}
 regexper='^(https?)://[a-zA-Z0-9.-]+\.[a-zA-Z0-9]{1,}(:[0-9]{1,5})?(/[^/]*)*$'
 function Diy_Download(){
+	if [ -f "${dir}/$(basename ${1})" ]; then
+		echo -e "$(date '+%Y-%m-%d %H:%M:%S')\e[1;32m - 【$(basename ${1})】文件已存在\e[0m"
+		return 0
+	fi
 	http_code=$(curl -o /dev/null -s --head -w "%{http_code}" "${url}")
 	if [ "${http_code}" -ge 400 ] || [ "${http_code}" = "000" ]; then
 		echo "无法访问或链接失效，状态码: ${http_code}"
